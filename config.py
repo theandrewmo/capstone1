7b6c910420c06f8a8dfe6484c24f00c2410f883b
Andrew Mo <106348010+theandrewmo@users.noreply.github.com>
2023-05-30 16:49:18 -0600

    update for config

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ class Config(object):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
+    FIREBASE_API_KEY = os.environ.get('FIREBASE_API_KEY')
 
 class ProductionConfig(Config):
     WTF_CSRF_ENABLED = False
