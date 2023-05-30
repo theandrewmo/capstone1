ad0fa29cbb82ed34f45c131d1be2aab41d964378
Andrew Mo <106348010+theandrewmo@users.noreply.github.com>
2023-05-30 17:33:03 -0600

    adding yml and a remote db

--- test_runner.py
+++ test_runner.py
@@ -1,11 +1,11 @@
 import unittest
-from test_brewery_model import BreweryModelTestCase
+from test_user_model import UserModelTestCase
 
 # Create a TestSuite
 test_suite = unittest.TestSuite()
 
 # Add your test class to the TestSuite
-test_suite.addTest(unittest.makeSuite(BreweryModelTestCase))
+test_suite.addTest(unittest.makeSuite(UserModelTestCase))
 
 # Run the tests
 runner = unittest.TextTestRunner()
